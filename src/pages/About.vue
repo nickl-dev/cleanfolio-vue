a1640f4afde8edb981d6689306c2b9af0b85ad74
Nicholas Lal <nlalkta@gmail.com>
2022-12-09 19:54:37 -0800

    -replaced resume -updated links

--- src/pages/About.vue
+++ src/pages/About.vue
@@ -5,58 +5,46 @@
       <h1 class="about__name">Hi, I'm {Your Name}</h1>
       <div class="about__occupation-wrapper">
         <h2 class="about__occupation">I'm a Developer</h2>
-      </div>    
+      </div>
     </header>
 
-    <section class="about__wrapper">
-      <div class="about__subwrapper">
-        <img 
-          :src="personalPicture" 
-          alt="Picture of yourself" 
-          class="about__image" 
-          loading="lazy"
-        />
-        <div class="about__biowrapper">
-          <!-- Replace this text with a short bio about yourself -->
-          <p class="about__bio">
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
-          </p>
-          <a
-            :href="resume"
-            class="about__resume"
-            target="_blank"
-          >
-          Resume
-          </a>
-        </div>
-      </div>
+    <section class="about__bio-wrapper">
+      <!-- Replace this text with a short bio about yourself -->
+      <p class="about__bio">
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+        commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+        velit esse cillum dolore eu fugiat nulla pariatur.
+      </p>
+      <a :href="resume" class="about__resume" target="_blank"> Resume </a>
     </section>
   </div>
 </template>
 
 <script>
-// Replace with a photo of yourself which can be stored in @/assets
-import personalPicture from '@/assets/personal-picture.jpg'
-
 export default {
-  name: "About",
+  name: 'About',
 
-  data () {
+  data() {
     return {
       // Replace with a link to your resume which can be stored in /public
-      resume: '../resume.pdf',
-      personalPicture: personalPicture
-    }
+      resume: '../resume.pdf'
+    };
   }
-}
+};
 </script>
 
 <style lang="scss">
-@import "@/styles/global.scss";
+@import '@/styles/global.scss';
 
 @keyframes type {
-  from { width: 0; }
-  to { width: 100%; }
+  from {
+    width: 0;
+  }
+  to {
+    width: 100%;
+  }
 }
 
 .about {
@@ -68,8 +56,12 @@ export default {
 
   &__name {
     font-size: 40px;
-    @include tablet { font-size: 70px; }
-    @include desktop { font-size: 100px; }
+    @include tablet {
+      font-size: 70px;
+    }
+    @include desktop {
+      font-size: 100px;
+    }
   }
 
   &__occupation {
@@ -79,44 +71,50 @@ export default {
     font-size: $font20;
     font-weight: 600;
     margin: 20px auto 0 auto;
-    @include tablet { font-size: 25px; }
-    &-wrapper { display: inline-block; }
-  }
-  
-  &__wrapper {
-    background: $stockBlack;
-    padding: 50px $padding5;
-    margin-top: 43vh;
-    @include tablet { padding: 75px $padding5; }
-    @include desktop { padding: 100px 13%; }
-  }
-
-  &__subwrapper {
-    text-align: center;
     @include tablet {
-      @include flexCenter;
-      justify-content: space-around;
+      font-size: 25px;
+    }
+    &-wrapper {
+      display: inline-block;
     }
   }
 
-  &__biowrapper {
-    max-width: 550px;
-    margin: auto;
-    @include tablet { width: 50%; padding: 0 20px; margin: 0; }
-    @include desktop { padding: 0; }
-  }
+  // &__wrapper {
+  //   background: $stockBlack;
+  //   padding: 50px $padding5;
+  //   margin-top: 43vh;
+  //   @include tablet {
+  //     padding: 75px $padding5;
+  //   }
+  //   @include desktop {
+  //     padding: 100px 13%;
+  //   }
+  // }
 
-  &__image {
-    width: 100%;
-    max-width: 350px;
-  }
+  // &__subwrapper {
+  //   text-align: center;
+  //   @include tablet {
+  //     @include flexCenter;
+  //     justify-content: space-around;
+  //   }
+  // }
 
+  &__bio-wrapper {
+    background: $stockBlack;
+    padding: 50px $padding5;
+    margin-top: 45vh;
+    text-align: center;
+    @include tablet {
+      padding: 75px $padding5;
+    }
+  }
   &__bio {
+    max-width: 600px;
     color: $stockWhite;
     font-size: $font16;
-    margin: 20px 0;
+    margin: 20px auto 30px auto;
   }
-  
+
   &__resume {
     @include button;
     display: inline-block;
--- src/pages/Contact.vue
+++ src/pages/Contact.vue
@@ -1,9 +1,7 @@
 <template>
   <div class="contact">
     <h1 class="contact__heading">Contact</h1>
-    <p class="contact__thankyou">
-      Thank you for viewing my portfolio!
-    </p>
+    <p class="contact__thankyou">Thank you for viewing my portfolio!</p>
 
     <div class="contact__links">
       <a :href="linkedin" class="contact__link">
@@ -17,31 +15,31 @@
 </template>
 
 <script>
-import LinkedInIcon from "vue-material-design-icons/Linkedin.vue"
-import GitHubIcon from "vue-material-design-icons/Github.vue"
+import LinkedInIcon from 'vue-material-design-icons/Linkedin.vue';
+import GitHubIcon from 'vue-material-design-icons/Github.vue';
 
 export default {
-  name: "Contact",
+  name: 'Contact',
 
   components: {
     LinkedInIcon,
     GitHubIcon
   },
 
-  data () {
+  data() {
     return {
       iconSize: 45,
       // Replace with your LinkedIn profile link
-      linkedin: "https://nlaldevcleanfolio.netlify.app/",
+      linkedin: 'https://nickl-dev-cleanfolio.netlify.app/',
       // Replace with your GitHub profile link
-      github: "https://nlaldevcleanfolio.netlify.app/",
-    }
+      github: 'https://nickl-dev-cleanfolio.netlify.app/'
+    };
   }
-}
+};
 </script>
 
 <style lang="scss">
-@import "@/styles/global.scss";
+@import '@/styles/global.scss';
 .contact {
   @include flexCenter;
   @include padding5;
@@ -55,7 +53,9 @@ export default {
   &__link {
     @include iconLink;
     margin: 0 10px;
-    &:hover { box-shadow: 0 3px 2px -2px rgba($stockBlack, 1); }
+    &:hover {
+      box-shadow: 0 3px 2px -2px rgba($stockBlack, 1);
+    }
   }
 }
 </style>
